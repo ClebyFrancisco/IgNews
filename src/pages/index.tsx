90967b5439abed0cb556e589a052ef2bede1a8ce
Cleby Francisco <clebysilva5@gmail.com>
2021-11-27 23:16:46 -0300

    home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,25 @@
 
-import Head  from "next/head"
+import Head  from "next/head";
+
+import styles from "./home.module.scss";
 
 export default function Home() {
   return (
     <>
-    <Head>
-      <title> Inicio - Ig.News</title>
-
-    </Head>
-    <h1 >
-      Hello world
-    </h1>
+      <Head>
+        <title> Home | Ig.News</title>
+      </Head>
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👏 Hey, welcome!</span>
+          <h1>News about the<span> React</span> world.</h1>
+          <p>
+            get acess to all the publications <br />
+            <span> for $9.90 month</span>
+          </p>
+        </section>
+        <img src="/images/avatar.svg" alt="Girl Coding" />
+      </main>
     </>
   );
 }
