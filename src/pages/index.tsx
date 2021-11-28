afd36e9315e899ee05886bbe2688dc29f1f3f190
Cleby Francisco <clebysilva5@gmail.com>
2021-11-27 23:28:24 -0300

    Component: SubscribeButton

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 
 import Head  from "next/head";
+import { SubscribeButton } from "../components/SubscribeButton/SubscribeButton";
 
 import styles from "./home.module.scss";
 
@@ -17,6 +18,7 @@ export default function Home() {
             get acess to all the publications <br />
             <span> for $9.90 month</span>
           </p>
+          <SubscribeButton />
         </section>
         <img src="/images/avatar.svg" alt="Girl Coding" />
       </main>
