2379a0a5fe47237c23eeaf17d048432799047b5b
Cleby Francisco <clebysilva5@gmail.com>
2021-12-03 22:02:40 -0300

    Static Site Generation (SSG)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,14 @@
-import { GetServerSideProps } from "next";
+import { GetStaticProps } from "next";
 import Head from "next/head";
 import { SubscribeButton } from "../components/SubscribeButton/SubscribeButton";
 import { stripe } from "../services/stripe";
 
 import styles from "./home.module.scss";
+
+
+// Client-Side
+// Server-Side
+// Static Site Generation
 
 interface HomeProps {
   product: {
@@ -35,20 +40,21 @@ export default function Home({ product }: HomeProps) {
     </>
   );
 }
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const price = await stripe.prices.retrieve("price_1K2ldZLdnB6uwEZeuC9RPQrj");
 
   const product = {
     priceId: price.id,
-    amount: new Intl.NumberFormat('en-US', {
-      style:'currency',
-      currency:'USD',
+    amount: new Intl.NumberFormat("en-US", {
+      style: "currency",
+      currency: "USD",
     }).format(price.unit_amount / 100),
   };
-  
+
   return {
     props: {
       product,
     },
+    revalidate: 60 * 60 * 24, //24hours
   };
 };
