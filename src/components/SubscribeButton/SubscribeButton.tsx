1d5d6760658d6872fb390b455a77e8bc906263cc
Cleby Francisco <clebysilva5@gmail.com>
2021-12-14 22:27:44 -0300

    Generating checkout session

--- src/components/SubscribeButton/SubscribeButton.tsx
+++ src/components/SubscribeButton/SubscribeButton.tsx
@@ -1,3 +1,5 @@
+import { useSession, signIn } from "next-auth/client";
+import { SignInButton } from "../SignInButton";
 import styles from "./styles.module.scss";
 
 interface SubscribeButtonProps {
@@ -5,10 +7,22 @@ interface SubscribeButtonProps {
 }
 
 export function SubscribeButton({ priceId }: SubscribeButtonProps){
+
+    const [session] = useSession();
+
+    function handleSubscribe(){
+        if(!session){
+            signIn('github')
+            return
+        }
+        
+
+    }
     return(
         <button
         type="button"
         className={styles.subscribeButton}
+        onClick={handleSubscribe}
         >
             Subscribe Now!
         </button>
