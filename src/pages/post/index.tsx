b38c6a06dc627eb1967d0eaab9b8e9b38f2d9e6c
Cleby Francisco <clebysilva5@gmail.com>
2021-12-22 22:33:39 -0300

    Consumindo API do Prismic

--- src/pages/post/index.tsx
+++ src/pages/post/index.tsx
@@ -1,46 +1,69 @@
-import  Head  from 'next/head';
-import styles from './styles.module.scss';
+import Prismic from "@prismicio/client";
+import { GetStaticProps } from "next";
+import Head from "next/head";
+import { getPrismicClient } from "../../services/prismic";
+import styles from "./styles.module.scss";
 
-export default function Posts(){
-    return (
-      <>
-        <Head>
-          <title>Post | Ignews </title>
-        </Head>
-        <main className={styles.container}>
-          <div className={styles.posts}>
-            <a href="#">
-              <time> 12 de março de 2021</time>
-              <strong>Lorem ipsum dolor sit, adipisicing elit.</strong>
-              <p>
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ea
-                itaque nam eius beatae consequuntur iste expedita, laboriosam
-                pariatur sed odio. Eos reprehenderit animi est quaerat rem sit
-                nesciunt vitae maxime.
-              </p>
-            </a>
-            <a href="#">
-              <time> 12 de março de 2021</time>
-              <strong>Lorem ipsum dolor sit, adipisicing elit.</strong>
-              <p>
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ea
-                itaque nam eius beatae consequuntur iste expedita, laboriosam
-                pariatur sed odio. Eos reprehenderit animi est quaerat rem sit
-                nesciunt vitae maxime.
-              </p>
-            </a>
-            <a href="#">
-              <time> 12 de março de 2021</time>
-              <strong>Lorem ipsum dolor sit, adipisicing elit.</strong>
-              <p>
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ea
-                itaque nam eius beatae consequuntur iste expedita, laboriosam
-                pariatur sed odio. Eos reprehenderit animi est quaerat rem sit
-                nesciunt vitae maxime.
-              </p>
-            </a>
-          </div>
-        </main>
-      </>
-    );
+export default function Posts() {
+  return (
+    <>
+      <Head>
+        <title>Post | Ignews </title>
+      </Head>
+      <main className={styles.container}>
+        <div className={styles.posts}>
+          <a href="#">
+            <time> 12 de março de 2021</time>
+            <strong>Lorem ipsum dolor sit, adipisicing elit.</strong>
+            <p>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ea
+              itaque nam eius beatae consequuntur iste expedita, laboriosam
+              pariatur sed odio. Eos reprehenderit animi est quaerat rem sit
+              nesciunt vitae maxime.
+            </p>
+          </a>
+          <a href="#">
+            <time> 12 de março de 2021</time>
+            <strong>Lorem ipsum dolor sit, adipisicing elit.</strong>
+            <p>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ea
+              itaque nam eius beatae consequuntur iste expedita, laboriosam
+              pariatur sed odio. Eos reprehenderit animi est quaerat rem sit
+              nesciunt vitae maxime.
+            </p>
+          </a>
+          <a href="#">
+            <time> 12 de março de 2021</time>
+            <strong>Lorem ipsum dolor sit, adipisicing elit.</strong>
+            <p>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ea
+              itaque nam eius beatae consequuntur iste expedita, laboriosam
+              pariatur sed odio. Eos reprehenderit animi est quaerat rem sit
+              nesciunt vitae maxime.
+            </p>
+          </a>
+        </div>
+      </main>
+    </>
+  );
 }
+
+export const getStaticProps: GetStaticProps = async () => {
+  const prismic = getPrismicClient();
+
+  const response = await prismic.query([
+    Prismic.predicates.at('document.type', 'publication')
+  ], {
+    fetch: ['publication.title', 'publication.content'],
+    pageSize: 100,
+  }
+    
+  );
+
+  console.log(JSON.stringify(response, null, 2))
+  return {
+    props: {
+
+    }
+  }
+};
