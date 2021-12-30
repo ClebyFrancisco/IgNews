81c3d972f98fbc25405a2b8436575db8547fb08b
Cleby Francisco <clebysilva5@gmail.com>
2021-12-29 21:57:51 -0300

    component ActiveLink

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,22 +1,22 @@
-import  Link  from "next/link";
+import { ActiveLink } from "../ActiveLink";
 import { SignInButton } from "../SignInButton";
 import styles from "./styles.module.scss";
 
 export function Header() {
-    return (
-      <header className={styles.headerContainer}>
-        <div className={styles.headerContent}>
-          <img src="/images/logo.svg" alt="Ig.news" />
-          <nav>
-            <Link href="/">
-              <a className={styles.active}>Home</a>
-            </Link>
-            <Link href="/posts" prefetch>
-              <a>Post</a>
-            </Link>
-          </nav>
-          <SignInButton />
-        </div>
-      </header>
-    );
-};
+  return (
+    <header className={styles.headerContainer}>
+      <div className={styles.headerContent}>
+        <img src="/images/logo.svg" alt="Ig.news" />
+        <nav>
+          <ActiveLink href="/" activeClassName={styles.active}>
+            <a>Home</a>
+          </ActiveLink>
+          <ActiveLink href="/posts" activeClassName={styles.active} prefetch>
+            <a>Post</a>
+          </ActiveLink>
+        </nav>
+        <SignInButton />
+      </div>
+    </header>
+  );
+}
